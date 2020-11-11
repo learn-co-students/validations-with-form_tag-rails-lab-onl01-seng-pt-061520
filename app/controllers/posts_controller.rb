424da7a607fb11a8a4fa2553a1e435457abd2e94
Matt Gordon <bigmattsize21@gmail.com>
2020-11-11 21:24:23 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,19 @@ class PostsController < ApplicationController
 
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
+  
+  def new
+  end
+
+  def create
+  end
+
 
   private
 
